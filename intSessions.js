1343c66b1c318d73c86d3e8c4cfec0d722d058ee
dchen21 <zdent09@gmail.com>
2018-04-07 23:08:19 +0800

    fixed bug in setting dynamic port

--- intSessions.js
+++ intSessions.js
@@ -157,6 +157,6 @@ app.use(function(err, req, res, next){
   res.render('500');
 });
 
-app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+app.listen(port, function(){
+  console.log('App started on http://localhost:' + port);
 });
